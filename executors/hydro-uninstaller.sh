5377331ce723e5e37f8124d29073a7d41410e075
void <87148335+nullstate9@users.noreply.github.com>
2025-05-01 17:07:14 +0100

    Update hydro-uninstaller.sh
    Added reinstall function

--- executors/hydro-uninstaller.sh
+++ executors/hydro-uninstaller.sh
@@ -6,16 +6,17 @@ main() {
     sleep 1
     echo "Install Hydrogen-M:         Press 1"
     echo "Uninstall Hydrogen-M:       Press 2"
+    echo "Reinstall Hydrogen-M:       Press 3"
     echo "Exit:                       Press Any Key"
 
     read -n 1 -s user_input
 
     if [ "$user_input" == "1" ]; then
-        echo "Installing Hydrogen"
+        echo "[!] Installing Hydrogen"
         bash -c "$(curl -fsSL https://hydrogen.lat/install.hydrogen)"
         
     elif [ "$user_input" == "2" ]; then
-        echo "Uninstalling Hydrogen-M"
+        echo "[!] Uninstalling Hydrogen-M"
 
         if [ -d "/Applications/Hydrogen-M.app" ]; then
             echo "[-] Removing /Applications/Hydrogen-M.app..."
@@ -72,6 +73,63 @@ main() {
         rm -rf hydro-uninstaller.sh
 
         exit 0
+        
+    elif [ "$user_input" == "3" ]; then
+        echo "Uninstalling Hydrogen-M"
+
+        if [ -d "/Applications/Hydrogen-M.app" ]; then
+            echo "[-] Removing /Applications/Hydrogen-M.app..."
+            rm -rf "/Applications/Hydrogen-M.app"
+        else
+            echo "[!] Hydrogen Application not found!"
+        fi
+        
+        if [ -d "~/Hydrogen" ]; then
+            echo "[-] Removing ~/Hydrogen..."
+            rm -rf "~/Hydrogen"
+        else
+            echo "[!] Hydrogen folder not found!"
+        fi
+        
+        if [ -d "/Applications/Roblox.app" ]; then
+            echo "[-] Removing /Applications/Roblox.app..."
+            rm -rf "/Applications/Roblox.app"
+        else
+            echo "[!] Roblox app not found!"
+        fi
+
+        
+
+        cd /tmp
+        echo "[+] Downloading Latest Roblox..."
+
+        [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
+
+        arch=$(uname -m)
+        
+        if [[ "$arch" == "arm64" ]]; then
+            echo "[!] Apple Silicon CPU Detected"
+            version=$(curl -s http://setup.roblox.com/mac/arm64/DeployHistory.txt | grep "New Client version" | tail -n 1 | sed -n 's/.*\(version-[^ ]*\).*/\1/p')
+            curl -s "http://setup.rbxcdn.com/mac/arm64/{$version}-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+        elif [[ "$arch" == "x86_64" ]]; then
+            echo "[!] Intel CPU Detected"
+            version=$(curl -s http://setup.roblox.com/mac/DeployHistory.txt | grep "New Client version" | tail -n 1 | sed -n 's/.*\(version-[^ ]*\).*/\1/p')
+            curl -s "http://setup.rbxcdn.com/mac/{$version}-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+        else
+            echo "Unknown architecture: $arch. Aborting"
+            exit 0
+        fi
+        
+        echo "[+] Installing Latest Roblox..."
+
+        unzip -qo "./RobloxPlayer.zip"
+        mv ./RobloxPlayer.app /Applications/Roblox.app
+        rm ./RobloxPlayer.zip
+
+        sleep 1
+        echo "[+] Reinstalling Hydrogen"
+
+        bash -c "$(curl -fsSL https://hydrogen.lat/install.hydrogen)"
     else
         echo "Have a nice day!"
         rm -rf hydro-uninstaller.sh
